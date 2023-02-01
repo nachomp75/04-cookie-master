b6276310f04896245ab2f57a0aae942e604a6100
Nacho <nacho75xr@gmail.com>
2023-02-01 23:39:49 +0100

    Section 11: Cookies

---
+++ pages/theme-changer.tsx
@@ -0,0 +1,81 @@
+import { ChangeEvent, FC, useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
+
+import axios from 'axios';
+import {
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from '@mui/material';
+import Cookies from 'js-cookie';
+
+import { Layout } from '@/components/layouts';
+
+interface Props {
+  theme: string;
+}
+
+const ThemeChangerPage: FC<Props> = ({ theme }) => {
+  const [currentTheme, setCurrentTheme] = useState(theme);
+
+  const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedTheme = event.target.value;
+    setCurrentTheme(selectedTheme);
+
+    localStorage.setItem('theme', selectedTheme);
+    Cookies.set('theme', selectedTheme);
+  };
+
+  const onClick = async () => {
+    const { data } = await axios.get('/api/hello');
+  };
+
+  useEffect(() => {}, []);
+
+  return (
+    <Layout>
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>Theme</FormLabel>
+            <RadioGroup value={currentTheme} onChange={onThemeChange}>
+              <FormControlLabel
+                value='light'
+                control={<Radio />}
+                label='Light'
+              />
+              <FormControlLabel value='dark' control={<Radio />} label='Dark' />
+              <FormControlLabel
+                value='custom'
+                control={<Radio />}
+                label='Custom'
+              />
+            </RadioGroup>
+          </FormControl>
+
+          <Button onClick={onClick}>Request</Button>
+        </CardContent>
+      </Card>
+    </Layout>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { theme = 'light', name = 'No name' } = req.cookies;
+
+  const validThemes = ['light', 'dark', 'custom'];
+
+  return {
+    props: {
+      theme: validThemes.includes(theme) ? theme : 'dark',
+      name,
+    },
+  };
+};
+
+export default ThemeChangerPage;
